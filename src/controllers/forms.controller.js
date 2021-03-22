11735b8c352856fd2a6486a516d22322c92a2f3a
Thomas Boda <boda.thomas@orange.fr>
2021-03-22 01:13:53 +0100

    Add the createForm and the getForms routes

--- src/controllers/forms.controller.js
+++ src/controllers/forms.controller.js
@@ -1,11 +1,32 @@
-const form = {
-    name: new String(),
-    id: new String(),
-    createdAt: new Date()
-}
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'tellMeMore';
+const url = 'mongodb://localhost:27017' + dbName;
+let db
+
+MongoClient.connect(url, (err, client) => {
+   console.log("Successfully connected to the server")
+   db = client.db(dbName)
+   if(err) throw err
+   db.createCollection('forms', {}, (err, _) => {
+        if (err) {}
+    })
+   db.createCollection('results', {}, (err, _) => {
+        if (err) {}
+    })
+})
 
 exports.createForm = (req, res) => {
-
+    if (!req.body.name || !req.body.data) {
+        res.status(400).json({message: 'Bad request'})
+        return
+    }
+    db.collection('forms').insertOne({
+        name: req.body.name,
+        createdAt: new Date(),
+        data: req.body.data
+    })
+        .then(doc => res.status(200).json({...doc.ops[0], message: 'Successfully created the form'}))
+    return
 }
 
 exports.updateForm = (req, res) => {
@@ -13,5 +34,18 @@ exports.updateForm = (req, res) => {
 }
 
 exports.deleteForm = (req, res) => {
+
+}
+
+exports.getForms = (req, res) => {
+    db.collection('forms').find({}).toArray()
+        .then(docs => res.status(200).json(docs))
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
+}
+
+exports.getFormData = (req, res) => {
 
 }
--- src/routes/forms.route.js
+++ src/routes/forms.route.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const forms = require('../controllers/forms.controller')
 
 router.post('/createForm', forms.createForm)
-router.update('/updateForm', forms.updateForm)
+router.post('/updateForm', forms.updateForm)
 router.delete('/deleteForm', forms.deleteForm)
+router.get('/getForms', forms.getForms)
+router.get('/getFormData', forms.getFormData)
 
 module.exports = router
